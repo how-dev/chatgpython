38a89d70d87d17c07c597f2e9521ae6058f0a7f1
how-dev <howard.medeiros@gmail.com>
2023-03-14 16:59:01 -0300

    Add CI to run tests and style-check

---
+++ CHANGELOG.md
@@ -0,0 +1,29 @@
+# Changelog
+
+All notable changes to this project will be documented in this file.
+
+The format is based on [Keep a Changelog](https://keepachangelog.com/en/1.0.0/),
+and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0.html).
+
+* The following tags must be used:
+    Added for new features.
+    Changed for changes in existing functionality.
+    Deprecated for soon-to-be removed features.
+    Removed for now removed features.
+    Fixed for any bug fixes.
+    Security in case of vulnerabilities.
+
+Format message: date_at issue_id message changes
+example: * date_at - Task 0002 - fixed some bug.
+
+## [Unreleased]
+
+### Changed
+
+### Added
+
+* 2023-03-14 - Add CI to run tests and style-check
+
+### Fixed
+
+### Removed
--- chatgptonic/integration.py
+++ chatgptonic/integration.py
@@ -9,47 +9,34 @@ class ChatGPTMessageException(Exception):
 
 class ChatGPT:
     def __init__(
-        self,
-        api_key: str,
-        chat_model: str = "gpt-3.5-turbo",
-        role: str = "user"
+        self, api_key: str, chat_model: str = "gpt-3.5-turbo", role: str = "user"
     ):
         self.api_key: str = api_key
 
         self.chat_model: str = chat_model
         self.role: str = role
-        self.chat_url: str = (
-            "https://api.openai.com/v1/chat/completions"
-        )
+        self.chat_url: str = "https://api.openai.com/v1/chat/completions"
 
     def _get_headers(self) -> dict:
         return {
             "Content-Type": "application/json",
-            "Authorization": f"Bearer {self.api_key}"
+            "Authorization": f"Bearer {self.api_key}",
         }
 
     def _get_body(self, message: str, creativity: float) -> dict:
         return {
             "model": self.chat_model,
             "messages": [{"role": self.role, "content": message}],
-            "temperature": creativity
+            "temperature": creativity,
         }
 
     def _get_request(self, message: str, creativity: float):
         body = self._get_body(message, creativity)
         headers = self._get_headers()
 
-        return {
-            "url": self.chat_url,
-            "data": json.dumps(body),
-            "headers": headers
-        }
+        return {"url": self.chat_url, "data": json.dumps(body), "headers": headers}
 
-    def send(
-        self,
-        message: str,
-        creativity: float = .7
-    ) -> dict:
+    def send(self, message: str, creativity: float = 0.7) -> dict:
         request = self._get_request(message, creativity)
 
         response = requests.post(**request)
@@ -64,11 +51,7 @@ class ChatGPT:
             f"Something went wrong with this error message: {errors}"
         )
 
-    def just_chat(
-        self,
-        message: str,
-        creativity: float = .7
-    ) -> str:
+    def just_chat(self, message: str, creativity: float = 0.7) -> str:
         response = self.send(message, creativity=creativity)
 
         return response["choices"][0]["message"]["content"].replace("\n", "")
